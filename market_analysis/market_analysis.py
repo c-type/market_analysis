57edebc8bf608eacc00a1837d5759c94a20e6852
ctype <ctypecodes@gmail.com>
2020-06-05 19:59:42 -0700

    added volatility plotting capability

--- market_analysis/market_analysis.py
+++ market_analysis/market_analysis.py
@@ -1,7 +1,7 @@
 import yfinance as yf
 import matplotlib.pyplot as plt
 
-with open('./data/default_tickers.txt') as ticker_file:
+with open('./data/tickers.txt') as ticker_file:
     tickers = [ticker.strip() for ticker in ticker_file.readlines()]
 
 
@@ -14,6 +14,18 @@ class Stock:
         
     def daily_volatility(self):
         self.hist['volatility'] = abs((self.hist.Low - self.hist.High) / self.hist.High)
+        
+    def plot_volatility(self):
+        fig, ax = plt.subplots()
+        self.hist['volatility'].plot(label='volatility')
+        quant99 = self.hist['volatility'].quantile(0.999)
+        ax.plot_date([self.hist.index.min(), self.hist.index.max()], [quant99, quant99],'-.', label=r'99.9% quantile $= {0:.3f}$'.format(quant99))
+        std_value = self.hist['volatility'].std()
+        ax.plot_date([self.hist.index.min(), self.hist.index.max()], [3*std_value, 3*std_value],'--', label=r'$3 \sigma = {0:.3f}$'.format(3*std_value))
+        plt.title(self.stock.ticker + ' Volatility')
+        plt.xlabel('Date')
+        plt.legend()
+        plt.show()
 
 stocks = {}
 for ticker in tickers:
